b6d292cf2797390e9b8957edc4577856be01eab7
ViktorHagstrom <73221759+ViktorHagstrom@users.noreply.github.com>
2021-03-29 10:38:38 +0200

    Add files via upload

---
+++ API/Orders/Api.php
@@ -0,0 +1,30 @@
+<?php
+include_once'Orders.php';
+
+header('Content-type: application/json; charset=UTF-8');
+
+$limit = isset($_GET['limit']) ? $_GET['limit'] : 10;
+
+$min = 1;
+$max = 10;
+
+if (filter_var($limit, FILTER_VALIDATE_INT, array("options" => array("min_range"=>$min, "max_range"=>$max))) === false) {
+
+  $products = array("error"=>"Limit must be between 1 and 10");
+  
+} else {
+  
+  $products = array();
+
+  for ($i=0; $i <$limit; $i++) { 
+     
+    $productObject = new Product($i);
+    $product = $productObject->toArray();
+    array_push($products,$product);
+  }
+  
+}
+$json = json_encode($products,JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT);
+  echo $json; 
+    
+?>
---
+++ API/Orders/Orders.php
@@ -0,0 +1,64 @@
+<?php
+include_once('arrays.php');
+
+class Product{
+
+    private $customerID;
+    private $orderID;
+    private $date;
+    private $price;
+    private $products;
+    private $address;
+
+
+   
+public function __construct($index){
+
+    $this->customerID = rand(1,50);
+    $this->orderID= rand(1,50);
+    $this->date = self::getDate(); 
+    $this->price = rand(5,500); 
+    $this->products = self::getProducts(); 
+    $this->address = self::getAddress();    
+}
+public static function getProducts(){
+  $array = array();
+  
+  $rand = rand(1,10);
+
+  for ($i=0; $i < $rand; $i++) { 
+      array_push($array ,rand(1,10));
+  }
+  return $array;
+    
+}
+public static function getAddress(){
+     
+    $address = "Lorem ipsum ";
+    $address .= rand(1,50);
+       
+    return $address;
+}
+public static function getDate()
+{
+    $min = strtotime("-1 month");
+    $max = strtotime("now");
+    $timestamp = rand($min, $max);
+    $date = date('Y-m-d H:i', $timestamp);
+    return $date;
+}
+ 
+public function toArray(){
+    
+    $array = array(
+        "customerID"          => $this->customerID,
+        "orderID"       => $this->orderID,
+        "date"         => $this->date,
+        "products"   => $this->products,
+        "price"   => $this->price,
+        "address"         => $this->address
+    );
+    return $array;
+}
+
+}
